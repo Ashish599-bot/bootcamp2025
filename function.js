467f259147f1dbcaaa04f801874852dc1fcdd4e8
Ashish <ashishsky619gmail.com>
2025-07-02 13:36:26 +0600

    Adds reduce function

--- function.js
+++ function.js
@@ -60,3 +60,8 @@ function stringToArray(string){
     return string.split(" ")
 }
 console.log(stringToArray("The boy is so"))
+
+function grow(x){
+  return x.reduce((product,current) => product * current,1);
+}
+console.log(grow([1,2,5,6]))
